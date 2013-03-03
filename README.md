100664afd4b3f75588b411a25d4eb56b78792aca
cancerhermit <cancerhermit@gmail.com>
2013-03-03 12:46:16 +0400

    "init"

---
+++ README.md
@@ -0,0 +1,20 @@
+Features
+----------
+
++   rsync folders on Sublime save
++   custom rsync commands support
+
+Platform
+----------
+**Mac OS**, **Linux**
+
+Installing
+----------
+Sublime package [Installation Instructions](https://cancerhermit.github.com/sublime-installation.html)
+
+Usage
+----------
+
+in project folder create **rsync.txt** with destignation folder, for example:
+
+    ~/Library/Application Support/Sublime Text 2/Packages
---
+++ rsync.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from os import popen
+from os.path import exists, join
+from sublime import active_window, error_message
+from sublime_plugin import EventListener
+
+def rsync(source,dest):
+    source=source.replace(" ","\\ ")
+    dest=dest.replace(" ","\\ ")
+    cmd="/usr/bin/rsync -rtu --delete %s %s" % (source,dest)
+    popen(cmd)
+
+class ShebangPythonListener(EventListener):
+    def rsync_sh(self,source):
+        pass
+
+    def rsync_txt(self,source):
+        f=join(source,"rsync.txt")
+        if exists(f):
+            dest=open(f).read()
+            rsync(source,dest)
+
+    def rsync_dir(self,source):
+        self.rsync_txt(source)
+        self.rsync_sh(source)
+
+    def on_post_save(self, view):
+        try:
+            for source in active_window().folders():
+                self.rsync_dir(source)
+        except Exception,e:
+            error_message(str(e))
+        
